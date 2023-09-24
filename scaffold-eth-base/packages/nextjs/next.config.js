cae516a5b86ba58b480965d4425e3c69681f6d63
Ubuntu <ubuntu@ip-172-31-18-154.us-west-2.compute.internal>
2023-09-24 00:53:02 +0000

    fix for submodule / conversion to mono repo

---
+++ scaffold-eth-base/packages/nextjs/next.config.js
@@ -0,0 +1,28 @@
+// @ts-check
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  typescript: {
+    ignoreBuildErrors: process.env.NEXT_PUBLIC_IGNORE_BUILD_ERROR === "true",
+  },
+  eslint: {
+    ignoreDuringBuilds: process.env.NEXT_PUBLIC_IGNORE_BUILD_ERROR === "true",
+  },
+  webpack: config => {
+    config.resolve.fallback = { fs: false, net: false, tls: false };
+    return config;
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'storage.unlock-protocol.com',
+        port: '',
+        pathname: '/*',
+      },
+    ],
+  }
+};
+
+module.exports = nextConfig;
