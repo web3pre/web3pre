54848a0203db2aad4bb67d6b153e9fcee9371c08
Ubuntu <ubuntu@ip-172-31-18-154.us-west-2.compute.internal>
2023-09-24 10:58:01 +0000

    webservice commit

--- scaffold-eth-base/packages/nextjs/components/example-ui/ContractData.tsx
+++ scaffold-eth-base/packages/nextjs/components/example-ui/ContractData.tsx
@@ -1,6 +1,8 @@
 import { useEffect, useRef, useState } from "react";
 import Marquee from "react-fast-marquee";
 import { useAccount } from "wagmi";
+import Link from 'next/link';
+
 import {
   useAnimationConfig,
   useScaffoldContract,
@@ -73,17 +75,17 @@ export const ContractData = () => {
   return (
     <div className="flex flex-col justify-center items-center bg-[url('/assets/gradient-bg.png')] bg-[length:100%_100%] py-10 px-5 sm:px-0 lg:py-auto max-w-[100vw] ">
         <div className="flex flex-col mt-6 px-7 py-8 bg-base-200 opacity-80 rounded-2xl shadow-lg border-2 border-primary">
-          <span className="text-4xl sm:text-6xl text-black">Purchase Zoom Ticket</span>
+          <span className="text-4xl sm:text-6xl text-black">Use Zoom Ticket Now</span>
 
           <div className="mt-8 flex flex-col sm:flex-row items-start sm:items-center gap-2 sm:gap-5">
             <div className="flex rounded-full border border-primary p-1 flex-shrink-0">
               <div className="flex rounded-full border-2 border-primary p-1">
-                <Link href="https://app.unlock-protocol.com/checkout?id=8b240557-7eaa-408e-b9ba-0aab9d954eaa" passHref>
+                <Link href="http://18.237.73.29:3000/members" passHref>
                 <button
                   className="btn btn-primary rounded-full capitalize font-normal font-white w-24 flex items-center gap-1 hover:gap-2 transition-all tracking-widest"
                 >
 
-                      Buy Now <ArrowSmallRightIcon className="w-3 h-3 mt-0.5" />
+                      Verify NFT
                      <>
                     </>
                 </button>
@@ -92,10 +94,6 @@ export const ContractData = () => {
             </div>
           </div>
 
-          <div className="mt-4 flex gap-2 items-start">
-            <span className="text-sm leading-tight">Price:</span>
-            <div className="badge badge-warning">0.00 BASE ETH + Gas</div>
-          </div>
         </div>
     </div>
   );
